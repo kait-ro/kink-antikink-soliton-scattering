190c71d400e8a704427176fd83f32bfdd2e6fa33
kait-ro <dabruanikait@gmail.com>
2026-07-27 21:16:28 +0530

    Add: Initial Kinks + Two-soliton initial condition

---
+++ src/kinkantikink/boost.py
@@ -0,0 +1,23 @@
+from parameters import Params
+from physics import phiPrime, staticKink
+
+
+def positionKinkBoostInitial(x, x0, v_in, p: Params, sign):
+    gamma = 1 / (1 - v_in**2) ** 0.5
+    return staticKink(gamma * x, gamma * x0, p, sign)
+
+def movingKinkInitial(x, x0, v_in,p: Params, sign):
+    gamma = 1 / (1 - v_in**2) ** 0.5
+    return -v_in * gamma * phiPrime(gamma * x, gamma * x0, sign)
+
+#Two soliton initial condition
+#Centered at 0, d0 apart
+def TwoSolitonIC(x, d0, v_in, p):
+    kink_part = positionKinkBoostInitial(x, -d0, v_in, p, sign=1)
+    antikink_part = positionKinkBoostInitial(x, d0, v_in, p, sign=1)
+    phi0 = kink_part - antikink_part - p.v
+
+    kink_vel = movingKinkInitial(x, -d0, v_in, p, sign=1)
+    antikink_vel = movingKinkInitial(x, d0, v_in, p, sign=1)
+    phi_dot0 = kink_vel + antikink_vel
+    return phi0, phi_dot0
