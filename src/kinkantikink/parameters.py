61734e18562c960d55e5f52386a9866598ce6919
kait-ro <dabruanikait@gmail.com>
2026-07-27 19:25:58 +0530

    style: parameters and physics

--- src/kinkantikink/parameters.py
+++ src/kinkantikink/parameters.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass(frozen=True)
 class Params:
     lam: float = 2.0
--- src/kinkantikink/physics.py
+++ src/kinkantikink/physics.py
@@ -1,25 +1,30 @@
+import numpy as np
 from parameters import Params
-import numpy as np 
- 
-def V(phi, p: Params):
-    return p.lam/4 * (phi**2 - p.v**2)**2 
-  
-def Vprime(phi, p: Params):
-    return p.lam*phi*(phi**2-p.v**2)
 
-def staticKink(x, x0, p: Params, sign=1): 
-    k = (p.lam/2)**0.5 * p.v
+
+def V(phi, p: Params):
+    return p.lam / 4 * (phi**2 - p.v**2) ** 2
+
+
+def Vprime(phi, p: Params):
+    return p.lam * phi * (phi**2 - p.v**2)
+
+
+def staticKink(x, x0, p: Params, sign=1):
+    k = (p.lam / 2) ** 0.5 * p.v
     u = k * (x - x0)
-    return sign*p.v*np.tanh(u) 
- 
+    return sign * p.v * np.tanh(u)
+
+
 def phiPrime(x, x0, p: Params, sign=1):
-    k = (p.lam/2)**0.5 * p.v 
+    k = (p.lam / 2) ** 0.5 * p.v
     u = k * (x - x0)
-    sech_u = 1/np.cosh(u)  
-    return sign * p.v * k * sech_u**2 
+    sech_u = 1 / np.cosh(u)
+    return sign * p.v * k * sech_u**2
+
 
 def phiPrime2(x, x0, p: Params, sign=1):
-    k = (p.lam/2)**0.5 * p.v 
-    u = k * (x - x0)   
-    sech_u = 1/np.cosh(u)  
-    return -p.lam*(p.v**3)*sech_u**2*np.tanh(u)*sign  
+    k = (p.lam / 2) ** 0.5 * p.v
+    u = k * (x - x0)
+    sech_u = 1 / np.cosh(u)
+    return -p.lam * (p.v**3) * sech_u**2 * np.tanh(u) * sign
