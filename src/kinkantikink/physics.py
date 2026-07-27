ef46d02de30522dfd9a2b2f0c2bf95ee630610fa
kait-ro <dabruanikait@gmail.com>
2026-07-27 09:50:00 +0530

    Add: Potential V, Vprime, staticKink

---
+++ src/kinkantikink/physics.py
@@ -0,0 +1,16 @@
+from parameters import Params
+import numpy as np 
+
+class Field:
+    def V(phi, p: Params):
+        return p.lam/4 * (phi**2 - p.v**2)**2
+
+    def Vprime(phi, p: Params):
+        return p.lam*phi*(phi**2-p.v**2)
+
+    def staticKink(x, x_0, p: Params, sign=1):
+        if sign==-1:
+            return p.v*np.tanh((p.lam/2)**(1/2)*p.v*(x-x_0))*(-1)
+        else:
+            p.v*np.tanh((p.lam/2)**(1/2)*p.v*(x-x_0))
+
