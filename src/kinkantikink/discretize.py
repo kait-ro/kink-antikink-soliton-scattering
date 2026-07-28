1383b7a4740baae1ac4058e431bfd9d18b5374d1
kait-ro <dabruanikait@gmail.com>
2026-07-28 21:32:34 +0530

    fix: shift parameter in laplacian

--- src/kinkantikink/discretize.py
+++ src/kinkantikink/discretize.py
@@ -10,10 +10,14 @@ def spatialGrid(p: Params):
 def gridToPhi(x, phi_func, x0, p: Params, sign=1):
     return phi_func(x, x0, p, sign)
 
-def laplacian(phiArr, dx):
+def laplacian(phiArr, dx, boundary="zero"):
     lap = np.zeros(len(phiArr))
-    # taylor approx
-    lap[1:-1] = (phiArr[2:] - 2 * phiArr[1:-1] + phiArr[:-2]) / dx**2 
+    lap[1:-1] = (phiArr[2:] - 2 * phiArr[1:-1] + phiArr[:-2]) / dx**2
+
+    if boundary == "shift":
+        lap[0] = (phiArr[2] - 2 * phiArr[1] + phiArr[0]) / dx**2
+        lap[-1] = (phiArr[-1] - 2 * phiArr[-2] + phiArr[-3]) / dx**2
+
     return lap
 
 def checkCFL(p: Params):
